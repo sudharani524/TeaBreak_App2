b58c227bcdc5786c730c0196fd542876e421cd60
Nandini59 <srinandini.mandala@gmail.com>
2024-01-09 19:10:51 +0530

    DispacterLogin

--- app/src/main/java/com/example/teabreak_app/Ui/Cartlist_Activity.java
+++ app/src/main/java/com/example/teabreak_app/Ui/Cartlist_Activity.java
@@ -113,9 +113,12 @@ public class Cartlist_Activity extends AppCompatActivity {
         binding.etSearchfilter.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+
             }
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+
             }
 
             @Override
